f30d898362d0b509c6f2826fd7cbdea205a02fe9
artmadeit <this.arthur.email@gmail.com>
2025-06-05 15:59:09 -0500

    feat: stub ingredients

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,15 @@ import Button from '@mui/material/Button';
 export default function Home() {
   return (
     <div>
-      hola mundo
-      <Button variant='contained'>some button</Button>
+      Persona 
+      arthur mauricio
+      
+      + alimento
+
+      alimento (autocomplete)
+      gramos
+      
+      <Button variant='contained'>Guardar</Button>
     </div>
   );
 }
