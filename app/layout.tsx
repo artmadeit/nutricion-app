723d981dbd8603522a3e4ef81358df0185819e46
artmadeit <this.arthur.email@gmail.com>
2025-06-05 15:54:25 -0500

    feat: add theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,21 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
 export const metadata: Metadata = {
   title: "Nutricion App",
   description: "App para registrar alimentos y sus valores nutritivos",
 };
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
 
 export default function RootLayout({
   children,
@@ -13,10 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" className={roboto.variable}>
       <body>
         <AppRouterCacheProvider>
-          {children}
+          <ThemeProvider theme={theme}>
+            {children}
+          </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
