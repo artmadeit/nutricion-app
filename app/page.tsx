0c9c07ebd16ff66a7b35a0bd1c4431af1a9e0d67
artmadeit <this.arthur.email@gmail.com>
2025-06-08 12:42:58 -0500

    feat: firstname and lastname

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,66 @@
+"use client";
+
 import Button from '@mui/material/Button';
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  FormContainer,
+  TextFieldElement,
+  useForm
+} from "react-hook-form-mui";
+import { Grid, Typography } from '@mui/material';
+
+const schema = z.object({
+  firstName: z.string().trim().min(1),
+  lastName: z.string().trim().min(1),
+});
 
 export default function Home() {
+  const formContext = useForm({
+    resolver: zodResolver(schema),
+  });
+
   return (
     <div>
-      Persona 
-      arthur mauricio
-      
-      + alimento
+      <FormContainer formContext={formContext} onSuccess={(values) => {
+        console.log(values)
+      }}>
+        <Grid container spacing={2} margin={4}>
+          <Grid size={12}>
+            <Typography variant="h5" gutterBottom>
+              Datos generales de persona
+            </Typography>
+          </Grid>
+          <Grid size={6}>
+            <TextFieldElement
+              fullWidth
+              name="firstName"
+              label="Nombre"
+              required
+            />
+          </Grid>
+          <Grid size={6}>
+            <TextFieldElement
+              fullWidth
+              name="lastName"
+              label="Apellido"
+              required
+            />
+          </Grid>
+          <Grid size={12}>
+            + alimento
 
-      alimento (autocomplete)
-      gramos
-      
-      <Button variant='contained'>Guardar</Button>
+            alimento (autocomplete)
+            gramos
+
+          </Grid>
+          <Grid size={12}>
+            <Button type="submit" variant="contained">
+              Guardar
+            </Button>
+          </Grid>
+        </Grid>
+      </FormContainer>
     </div>
   );
 }
