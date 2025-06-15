f603dd10fea886e8c18aa1118a95cea729ea56f4
artmadeit <this.arthur.email@gmail.com>
2025-06-14 21:20:24 -0500

    feat: array of food fields

--- app/(portal)/page.tsx
+++ app/(portal)/page.tsx
@@ -2,7 +2,8 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import AddIcon from '@mui/icons-material/Add';
-import { Box, Grid, TextField, Typography } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Fab, Grid, TextField, Tooltip, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import {
   DatePicker,
@@ -16,32 +17,35 @@ import {
   FormContainer,
   SelectElement,
   TextFieldElement,
+  useFieldArray,
   useForm,
 } from "react-hook-form-mui";
 import { z } from "zod";
 
 const schema = z.object({
+  code: z.string(),
   firstName: z.string().trim(),
   lastName: z.string().trim(),
   interviewDate: z.date(),
-  consumptionTime: z.date(),
-  origin: z.string(), // TODO: use enum,
   interviewNumber: z.string(),
-  code: z.string(),
-  weightInGrams: z
-    .number()
-    .min(0.1)
-    .max(999.9)
-    .refine((val) => Number.isInteger(val * 10), {
-      message: "Debe tener exactamente un decimal",
-    }),
-  weigthInGramsResidue: z
-    .number()
-    .min(0.0)
-    .max(999.9)
-    .refine((val) => Number.isInteger(val * 10), {
-      message: "Debe tener exactamente un decimal",
-    }),
+  foods: z.array(z.object({
+    origin: z.string(), // TODO: use enum,
+    consumptionTime: z.date(),
+    weightInGrams: z
+      .number()
+      .min(0.1)
+      .max(999.9)
+      .refine((val) => Number.isInteger(val * 10), {
+        message: "Debe tener exactamente un decimal",
+      }),
+    weigthInGramsResidue: z
+      .number()
+      .min(0.0)
+      .max(999.9)
+      .refine((val) => Number.isInteger(val * 10), {
+        message: "Debe tener exactamente un decimal",
+      }),
+  }))
 });
 
 const mealtimeOptions = [
@@ -99,27 +103,36 @@ const mealtime = (consumptionTime: Date) => {
   return mealTime?.label || mealtimeOptions[0].label;
 }
 
+const emptyFood = {
+  consumptionTime: new Date(),
+  origin: "",
+  weightInGrams: 0,
+  weigthInGramsResidue: 0
+}
+
 export default function Home() {
   const formContext = useForm({
     defaultValues: {
       interviewNumber: "001",
       interviewDate: new Date(),
-      consumptionTime: new Date()
+      foods: [
+        emptyFood
+      ]
     },
     resolver: zodResolver(schema),
   });
+
+  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray({
+    control: formContext.control,
+    name: "foods",
+  });
+
 
 
   const interviewDate = formContext.watch("interviewDate");
   const day = interviewDate ? format(interviewDate, "eeee", { locale: es }) : "";
 
   const codeRef = useMask({ mask: "______", replacement: { _: /\d/ } });
-
-  const weightInGrams = formContext.watch("weightInGrams");
-  const weigthInGramsResidue = formContext.watch("weigthInGramsResidue");
-  const quantityConsumed = (weightInGrams - weigthInGramsResidue || 0).toFixed(1);
-
-  const consumptionTime = formContext.watch("consumptionTime");
 
   return (
     <div>
@@ -226,113 +239,141 @@ export default function Home() {
               Datos generales de consumo de alimentos
             </Typography>
           </Grid>
-          <Grid container>
+          {
+            fields.map((field, index) => {
+              const weightInGrams = formContext.watch(`foods.${index}.weightInGrams`);
+              const weigthInGramsResidue = formContext.watch(`foods.${index}.weigthInGramsResidue`);
+              const quantityConsumed = (weightInGrams - weigthInGramsResidue || 0).toFixed(1);
+              const consumptionTime = formContext.watch(`foods.${index}.consumptionTime`);
 
-            <Grid size={6}>
-              <TextFieldElement
-                fullWidth
-                name="preparationCode"
-                label="Código de forma de preparación"
-              />
-            </Grid>
-            <Grid size={6}>
-              <TextFieldElement
-                fullWidth
-                name="preparationName"
-                label="Nombre de la preparación"
-              />
-            </Grid>
-            <Grid size={4}>
-              <TimePicker
-                label="Hora de consumo"
-                ampm
-                sx={{ width: "100%" }}
-                value={consumptionTime}
-                onChange={(value) => {
-                  if (value !== null) {
-                    formContext.setValue("consumptionTime", value);
-                  }
-                }}
-                viewRenderers={{
-                  hours: renderTimeViewClock,
-                  minutes: renderTimeViewClock,
-                  seconds: renderTimeViewClock,
-                }}
-              />
-            </Grid>
-            <Grid size={4}>
-              <TextField
-                label="Tiempo de comida"
-                variant="outlined"
-                value={mealtime(consumptionTime)}
-                disabled
-                fullWidth />
-            </Grid>
-            <Grid size={4}>
-              <SelectElement
-                label="Procedencia"
-                name="origin"
-                options={[
-                  {
-                    id: "1",
-                    label: "Casa",
-                  },
-                  {
-                    id: "2",
-                    label: "Vecino",
-                  },
-                  {
-                    id: "3",
-                    label: "Familiar",
-                  },
-                  {
-                    id: "4",
-                    label: "Comedor popular",
-                  },
-                  {
-                    id: "5",
-                    label: "Ambulante",
-                  },
-                  {
-                    id: "6",
-                    label: "Kiosko",
-                  },
-                  {
-                    id: "7",
-                    label: "Tienda/super",
-                  },
-                  {
-                    id: "77",
-                    label: "Otro", // TODO: add observation field
-                  },
-                  {
-                    id: "99",
-                    label: "No sabe",
-                  },
-                ]}
-                fullWidth
-              />
-            </Grid>
-            <Grid container sx={{paddingLeft: 4}}>
-              <Grid size={2}>
-                {/* TODO: automatic */}
-                <TextFieldElement
-                  fullWidth
-                  name="orderFood"
-                  label="N° Orden de Alimento"
-                />
-              </Grid>
-              <Grid size={4}>
-                <TextFieldElement
-                  fullWidth
-                  name="foodTableCode"
-                  label="Código alimento tabla"
-                  disabled
-                //TODO: automatic
-                />
-              </Grid>
-              <Grid size={6}>
-                {/* <Autocomplete
+              return (
+                <Grid container key={index}>
+                  <Grid size={5}>
+                    <TextFieldElement
+                      fullWidth
+                      name={`food.${index}.preparationCode`}
+                      label="Código de forma de preparación"
+                    />
+                  </Grid>
+                  <Grid size={6}>
+                    <TextFieldElement
+                      fullWidth
+                      name={`food.${index}.preparationName`}
+                      label="Nombre de la preparación"
+                    />
+                  </Grid>
+                  <Grid size={1} sx={{ display: "flex", justifyContent: "end", alignItems: "center" }}>
+                    {
+                      fields.length > 1 &&
+                      <Tooltip title="Eliminar preparación">
+                        <Fab
+                          size="small"
+                          onClick={() => remove(index)}
+                        >
+                          <DeleteIcon />
+                        </Fab>
+                      </Tooltip>}
+                  </Grid>
+                  <Grid size={4}>
+                    <TimePicker
+                      label="Hora de consumo"
+                      ampm
+                      sx={{ width: "100%" }}
+                      value={consumptionTime}
+                      onChange={(value) => {
+                        if (value !== null) {
+                          formContext.setValue(`foods.${index}.consumptionTime`, value);
+                        }
+                      }}
+                      viewRenderers={{
+                        hours: renderTimeViewClock,
+                        minutes: renderTimeViewClock,
+                        seconds: renderTimeViewClock,
+                      }}
+                    />
+                  </Grid>
+                  <Grid size={4}>
+                    <TextField
+                      label="Tiempo de comida"
+                      variant="outlined"
+                      value={mealtime(consumptionTime)}
+                      disabled
+                      fullWidth />
+                  </Grid>
+                  <Grid size={4}>
+                    <SelectElement
+                      label="Procedencia"
+                      name={`food.${index}.origin`}
+                      options={[
+                        {
+                          id: "1",
+                          label: "Casa",
+                        },
+                        {
+                          id: "2",
+                          label: "Vecino",
+                        },
+                        {
+                          id: "3",
+                          label: "Familiar",
+                        },
+                        {
+                          id: "4",
+                          label: "Comedor popular",
+                        },
+                        {
+                          id: "5",
+                          label: "Ambulante",
+                        },
+                        {
+                          id: "6",
+                          label: "Kiosko",
+                        },
+                        {
+                          id: "7",
+                          label: "Tienda/super",
+                        },
+                        {
+                          id: "77",
+                          label: "Otro", // TODO: add observation field
+                        },
+                        {
+                          id: "99",
+                          label: "No sabe",
+                        },
+                      ]}
+                      fullWidth
+                    />
+                  </Grid>
+                  <Grid container sx={{ paddingLeft: 4 }}>
+                    <Grid size={12}>
+                      <Typography
+                        variant="h6"
+                        gutterBottom
+                        style={{ paddingTop: "20px" }}
+                      >
+                        Ingredientes
+                      </Typography>
+                    </Grid>
+                    <Grid size={2}>
+                      {/* TODO: automatic */}
+                      <TextFieldElement
+                        fullWidth
+                        name={`food.${index}.orderFood`}
+                        label="N° Orden de Alimento"
+                      />
+                    </Grid>
+                    <Grid size={4}>
+                      <TextFieldElement
+                        fullWidth
+                        name={`food.${index}.foodTableCode`}
+                        label="Código alimento tabla"
+                        disabled
+                      //TODO: automatic
+                      />
+                    </Grid>
+                    <Grid size={6}>
+                      {/* <Autocomplete
                   disablePortal
                   options={top100Films}
                   sx={{ width: 300 }}
@@ -343,96 +384,108 @@ export default function Home() {
                     />
                   )}
                 /> */}
-                <TextFieldElement
-                  //TODO
-                  fullWidth
-                  name="foodIngredients"
-                  label="Ingrediente (nombre del Alimento)"
-                />
-              </Grid>
-              <Grid size={6}>
-                <TextFieldElement
-                  fullWidth
-                  name="portionServed"
-                  label="Porción servida (medida casera)"
-                />
-              </Grid>
-              <Grid size={6}>
-                <TextFieldElement
-                  fullWidth
-                  name="weightInGrams"
-                  label="Peso en gramos de la porción servida"
-                  required
-                  type="number"
-                />
-              </Grid>
-              <Grid size={6}>
-                <TextFieldElement
-                  fullWidth
-                  name="portionResidue"
-                  label="Residuo porción"
-                />
-              </Grid>
-              <Grid size={6}>
-                <TextFieldElement
-                  fullWidth
-                  name="weigthInGramsResidue"
-                  label="Peso en gramos del residuo de porción"
-                  type="number"
-                />
-              </Grid>
-              <Grid size={6}>
-                <TextField
-                  label="Cantidad consumida"
-                  variant="outlined"
-                  value={quantityConsumed}
-                  disabled
-                  fullWidth />
-              </Grid>
-              <Grid size={6}>
-                <SelectElement
-                  label="Fuente"
-                  name="source"
-                  options={[
-                    {
-                      id: "1",
-                      label: "Peso directo",
-                    },
-                    {
-                      id: "2",
-                      label: "Medida casera",
-                    },
-                    {
-                      id: "3",
-                      label: "P. c/agua",
-                    },
-                    {
-                      id: "4",
-                      label: "Foto atlas",
-                    },
-                    {
-                      id: "5",
-                      label: "Modelo",
-                    },
-                    {
-                      id: "6",
-                      label: "Comida del colegio",
-                    },
-                  ]}
-                  fullWidth
-                />
-              </Grid>
-              <Grid size={12}>
-                <Button variant="outlined" startIcon={<AddIcon />} fullWidth>
-                  Agregar ingrediente
-                </Button>
-              </Grid>
-            </Grid>
-            <Grid size={12}>
-              <Button variant="outlined" startIcon={<AddIcon />} fullWidth>
-                Agregar preparación
-              </Button>
-            </Grid>
+                      <TextFieldElement
+                        //TODO
+                        fullWidth
+                        name={`food.${index}.foodIngredients`}
+                        label="Ingrediente (nombre del Alimento)"
+                      />
+                    </Grid>
+                    <Grid size={6}>
+                      <TextFieldElement
+                        fullWidth
+                        name={`food.${index}.portionServed`}
+                        label="Porción servida (medida casera)"
+                      />
+                    </Grid>
+                    <Grid size={6}>
+                      <TextFieldElement
+                        fullWidth
+                        name={`food.${index}.weightInGrams`}
+                        label="Peso en gramos de la porción servida"
+                        required
+                        type="number"
+                      />
+                    </Grid>
+                    <Grid size={6}>
+                      <TextFieldElement
+                        fullWidth
+                        name={`food.${index}.portionResidue`}
+                        label="Residuo porción"
+                      />
+                    </Grid>
+                    <Grid size={6}>
+                      <TextFieldElement
+                        fullWidth
+                        name={`food.${index}.weigthInGramsResidue`}
+                        label="Peso en gramos del residuo de porción"
+                        type="number"
+                      />
+                    </Grid>
+                    <Grid size={6}>
+                      <TextField
+                        label="Cantidad consumida"
+                        variant="outlined"
+                        value={quantityConsumed}
+                        disabled
+                        fullWidth />
+                    </Grid>
+                    <Grid size={6}>
+                      <SelectElement
+                        label="Fuente"
+                        name={`food.${index}.source`}
+                        options={[
+                          {
+                            id: "1",
+                            label: "Peso directo",
+                          },
+                          {
+                            id: "2",
+                            label: "Medida casera",
+                          },
+                          {
+                            id: "3",
+                            label: "P. c/agua",
+                          },
+                          {
+                            id: "4",
+                            label: "Foto atlas",
+                          },
+                          {
+                            id: "5",
+                            label: "Modelo",
+                          },
+                          {
+                            id: "6",
+                            label: "Comida del colegio",
+                          },
+                        ]}
+                        fullWidth
+                      />
+                    </Grid>
+                    <Grid size={12}>
+                      <Button variant="outlined" startIcon={<AddIcon />} fullWidth>
+                        Agregar ingrediente
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              )
+            }
+            )
+          }
+
+          <Grid size={12}>
+            <Button
+              variant="outlined"
+              startIcon={<AddIcon />}
+              fullWidth
+              onClick={() => {
+                append(emptyFood)
+              }}
+            >
+              Agregar preparación
+            </Button>
           </Grid>
           <Grid size={12}>
             <Button type="submit" variant="contained">
