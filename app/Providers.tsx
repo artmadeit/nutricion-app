ff766670e331b7147fa8f609344224daf5240962
artmadeit <this.arthur.email@gmail.com>
2025-06-22 09:24:58 -0500

    feat: support swr

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -5,34 +5,26 @@ import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import { es as dateFnsEs } from "date-fns/locale/es";
 
 import React from "react";
-// import { SWRConfig } from "swr";
-// import { useAuthApi } from "../(api)/api";
-// import { AxiosRequestConfig } from "axios";
+import { SWRConfig } from "swr";
+import { api } from './(api)/api';
+import { AxiosRequestConfig } from 'axios';
 
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  // const getApi = useAuthApi();
+  const fetcher = (key: string | [string, AxiosRequestConfig]) => {
+    const [url, config] = typeof key === "string" ? [key] : key;
 
-  // const fetcher = (key: string | [string, AxiosRequestConfig]) => {
-  //   const [url, config] = typeof key === "string" ? [key] : key;
-
-  //   // if (config) {
-  //   //   config.paramsSerializer = (params: any) => {
-  //   //     return qs.stringify(params, { arrayFormat: "repeat" });
-  //   //   };
-  //   // }
-
-  //   return getApi().then((api) => api.get(url, config).then((res) => res.data));
-  // };
+    return api.get(url, config).then((res) => res.data);
+  };
 
   return (
-    // <SWRConfig value={{ fetcher }}>
-    <LocalizationProvider
-      dateAdapter={AdapterDateFns}
-      adapterLocale={dateFnsEs}
-    >
-      {children}
-    </LocalizationProvider>
-    // </SWRConfig>
+    <SWRConfig value={{ fetcher }}>
+      <LocalizationProvider
+        dateAdapter={AdapterDateFns}
+        adapterLocale={dateFnsEs}
+      >
+        {children}
+      </LocalizationProvider>
+    </SWRConfig>
   );
 };
