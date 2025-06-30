541445bb1304b5ec351c3ba95266d93cb6a6e395
Dede1421 <mauricioandre91@gmail.com>
2025-06-30 10:49:54 -0500

    create a SnackbarContext component

---
+++ app/(components)/SnackbarContext.tsx
@@ -0,0 +1,40 @@
+import { Snackbar } from "@mui/material";
+import React, { createContext } from "react";
+
+export const SnackbarContext = createContext({
+  message: "",
+  showMessage: (message: string) => {},
+});
+
+export const SnackbarProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [alertMessage, setAlertMessage] = React.useState<string>("");
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    setAlertMessage("");
+  };
+
+  const showAlertMessage = (message: string) => setAlertMessage(message);
+
+  return (
+    <SnackbarContext.Provider
+      value={{
+        message: alertMessage,
+        showMessage: showAlertMessage,
+      }}
+    >
+      <Snackbar
+        open={!!alertMessage}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message={alertMessage}
+      />
+    </SnackbarContext.Provider>
+  );
+};
